a6d98a2aff42c1aad387bed525877dbbb817bcf7
jala-asadli <jasadli16143@ada.edu.az>
2026-04-02 10:45:15 +0400

    Add CategoryEntity and CategoryRepository

--- src/main/java/az/edu/ada/wm2/lab6/model/Product.java
+++ src/main/java/az/edu/ada/wm2/lab6/model/Product.java
@@ -2,14 +2,27 @@ package az.edu.ada.wm2.lab6.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
 @Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
     @Id
@@ -19,45 +32,11 @@ public class Product {
     private BigDecimal price;
     private LocalDate expirationDate;
 
-    public Product() {
-    }
-
-    public Product(UUID id, String name, BigDecimal price, LocalDate expirationDate) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.expirationDate = expirationDate;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public LocalDate getExpirationDate() {
-        return expirationDate;
-    }
-
-    public void setExpirationDate(LocalDate expirationDate) {
-        this.expirationDate = expirationDate;
-    }
+    @ManyToMany
+    @JoinTable(
+            name = "product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
 }
